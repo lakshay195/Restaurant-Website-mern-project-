69a2ec5c2f7bf936cab7cf32c7218562832a4668
lakshay195 <lakshaybhandoria@gmail.com>
2024-08-09 13:52:39 +0530

    Add files via upload

---
+++ full/Backend/middleware/auth.js
@@ -0,0 +1,17 @@
+// middleware/authenticateToken.js
+
+const jwt = require('jsonwebtoken');
+
+const authenticateToken = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  const token = authHeader && authHeader.split(' ')[1];
+  if (!token) return res.sendStatus(401); // Unauthorized
+
+  jwt.verify(token, 'your-secret-key', (err, user) => {
+    if (err) return res.sendStatus(403); // Forbidden
+    req.user = user;
+    next();
+  });
+};
+
+module.exports = authenticateToken;
